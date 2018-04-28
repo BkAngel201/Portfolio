92e3908a7eb6a80915cf738d3b31d7b029064855
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2018-04-28 17:51:11 -0400

    mobile Version Done

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@ $("[data-toggle=navbar-collapse]").on("click",function(){
   let target = $(this).attr("data-target");
   $("#" + target).parents("nav").toggleClass("open");
 });
+$(".navbar ul.nav a:not([data-toggle=subnavbar-collapse])").on("click", function(){
+  $(this).parents("nav").removeClass("open");
+  $(this).parents("nav").find(".subnav").removeClass("open");
+});
+$("[data-toggle=subnavbar-collapse]").on("click",function(){
+  $(this).siblings("ul.subnav").toggleClass("open");
+});
+$("nav .subnav a").on("click", function(){
+  $(this).parents(".subnav").removeClass("open");
+});
