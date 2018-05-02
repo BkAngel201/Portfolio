a0b2c9a0d9b1839de21fb49e01e3f82a8d608734
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2018-05-02 02:01:38 -0400

    mostly finished

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ $("[data-toggle=navbar-collapse]").on("click",function(){
 $(".navbar ul.nav a:not([data-toggle=subnavbar-collapse])").on("click", function(){
   $(this).parents("nav").removeClass("open");
   $(this).parents("nav").find(".subnav").removeClass("open");
+  $(this).parents("nav").find("[data-toggle=subnavbar-collapse]").removeClass("active");
 });
 $("[data-toggle=subnavbar-collapse]").on("click",function(){
   $(this).siblings("ul.subnav").toggleClass("open");
+  $(this).toggleClass("active");
 });
 $("nav .subnav a").on("click", function(){
   $(this).parents(".subnav").removeClass("open");
